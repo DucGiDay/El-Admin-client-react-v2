61ae9957f2c81abe081f6be73f5f6d3e9f117490
TrungDuc <duc123zzmm@gmail.com>
2022-10-19 00:11:05 +0700

    hiển thị danh mục cơ bản

--- src/pages/components-overview/danh-muc/index.js
+++ src/pages/components-overview/danh-muc/index.js
@@ -5,6 +5,7 @@ import { Button } from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
+import TableDanhMuc from 'components/danh-muc/TableDanhMuc';
 
 function DanhMuc() {
   const [listDangKienThuc, setListDangKienThuc] = useState([])
@@ -13,19 +14,24 @@ function DanhMuc() {
   const [idDangKienThuc, setIdDangKienThuc] = useState('')
   const [idDonViKienThuc, setIdDonViKienThuc] = useState('')
 
-  async function queryDKT() {
+  async function getAllDKT() {
+    console.log(1)
     await axios
     .get(`http://localhost:4000/api/dkt`)
-    .then((response) => setListDangKienThuc(response.data))
+      .then((response) => {
+        console.log(2)
+        setListDangKienThuc(response.data)
+      })
     .catch(error => console.log(error))
+    console.log(3)
   }
-  async function queryDVKT() {
+  async function getAllDVKT() {
     await axios
     .get(`http://localhost:4000/api/dvkt`)
     .then((response) => setListDonViKienThuc(response.data))
     .catch(error => console.log(error))
   }
-  async function queryMTCT() {
+  async function getAllMTCT() {
     await axios
     .get(`http://localhost:4000/api/chi-tiet`)
     .then((response) => setListMoTaChiTiet(response.data))
@@ -33,18 +39,45 @@ function DanhMuc() {
   }
 
   useEffect(() => {
-    queryDKT()
-    queryDVKT()
-    queryMTCT()
+    getAllDKT()
+    getAllDVKT()
+    getAllMTCT()
   }, []);
+  const listDangKienThucObject = listDangKienThuc.map((response) => {
+    const dangKienThuc = JSON.parse(response)
+    return dangKienThuc
+  })
+  const listDonViKienThucObject = listDonViKienThuc.map((response) => {
+    const donViKienThuc = JSON.parse(response)
+    return donViKienThuc
+  })
+  const listMoTaChiTietObject = listMoTaChiTiet.map((response) => {
+    const moTaChiTiet = JSON.parse(response)
+    return moTaChiTiet
+  })
 
-  const test = async() => {
-    await console.log('listDangKienThuc', listDangKienThuc)
+  const test = () => {
+    console.log('listDangKienThucObject', listDangKienThucObject)
+    console.log('listDonViKienThucObject', listDonViKienThucObject)
+    console.log('listMoTaChiTietObject', listMoTaChiTietObject)
+  }
+
+  const tableDanhMuc = () => {
+    if (!idDangKienThuc) {
+      return (<TableDanhMuc props={listDangKienThucObject} danhMuc={ 'Dạng Kiến Thức' } />)
+    }
+    else if (idDangKienThuc && !idDonViKienThuc) {
+      const listDonViKienThucByIdDKT = listDonViKienThucObject.filter(item => item.Id_category_dkt === idDangKienThuc)
+      return(<TableDanhMuc props ={listDonViKienThucByIdDKT} danhMuc={ 'Đơn Vị Kiến Thức' }/>)
+
+    }
+    const listMoTaChiTietByIdDVKT = listMoTaChiTietObject.filter(item => item.Id_category_dvkt === idDonViKienThuc)
+    return(<TableDanhMuc props ={listMoTaChiTietByIdDVKT} danhMuc={ 'Mô Tả Chi Tiết' }/>)
   }
 
   return (
     <MainCard title="">
-      <Button onClick={test}>helo</Button>
+      {tableDanhMuc()}
     </MainCard>
   )
 }
