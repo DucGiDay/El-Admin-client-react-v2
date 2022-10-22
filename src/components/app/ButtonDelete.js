1763d017af1b92e04daf42b82d9b1bd48a024a4f
TrungDuc <duc123zzmm@gmail.com>
2022-10-23 01:39:09 +0700

    Thêm modal confirm lại thao tác xóa danh mục

--- src/components/app/ButtonDelete.js
+++ src/components/app/ButtonDelete.js
@@ -1,10 +1,21 @@
-import { Button} from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd';
 import axios from 'axios';
 import { NotificationManager} from 'react-notifications';
 
 
 function ButtonDelete(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    deleteDanhMucById()
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   const id = props.props
   const danhMuc = props.danhMuc
   let url = `http://localhost:4000/api/dkt/${id}`
@@ -22,8 +33,44 @@ function ButtonDelete(props) {
       )
   }
   return (
-    <Button onClick={deleteDanhMucById} danger>Delete</Button>
+    <>
+      <Button onClick={showModal} danger>Delete</Button>
+      <Modal title="Confirm modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <h2>{`Bạn có chắc chắn muốn xóa ${danhMuc} ${id}`}</h2>
+      </Modal>
+    </>
   )
 }
 
 export default ButtonDelete
+
+
+
+
+// import { Button, Modal } from 'antd';
+// import React, { useState } from 'react';
+// const App = () => {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const showModal = () => {
+//     setIsModalOpen(true);
+//   };
+//   const handleOk = () => {
+//     setIsModalOpen(false);
+//   };
+//   const handleCancel = () => {
+//     setIsModalOpen(false);
+//   };
+//   return (
+//     <>
+//       <Button type="primary" onClick={showModal}>
+//         Open Modal
+//       </Button>
+//       <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+//         <p>Some contents...</p>
+//         <p>Some contents...</p>
+//         <p>Some contents...</p>
+//       </Modal>
+//     </>
+//   );
+// };
+// export default App;
